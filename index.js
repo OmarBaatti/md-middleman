355db5a72ca78faea63f2366d755a11810b0c17f
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-12-07 15:54:24 +0600

    Add Discord bot command for ping response and update dependencies

---
+++ index.js
@@ -0,0 +1,35 @@
+const { REST, Routes, Client, GatewayIntentBits, Events } = require("discord.js")
+
+const commands = [
+  {
+    name: "ping",
+    description: "Sends back a pong response.",
+  }
+]
+
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_BOT_TOKEN)
+
+try {
+  console.log("Started refreshing application (/) commands.")
+  await rest.put(Routes.applicationCommands(process.env.DISCORD_CLIENT_ID), { body: commands });  
+
+  console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+  console.error(error);
+}
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on(Events.ClientReady, readyClient => {
+  console.log(`Logged in as ${readyClient.user.tag}!`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
