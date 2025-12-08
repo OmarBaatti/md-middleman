a55f2aa2966c4833af7ad1dda91642d788f77d4f
Omar-Baatti <baattiomar@gmail.com>
2025-12-08 13:54:21 +0100

    changed structure and modified details

--- forwarding/index.js
+++ forwarding/index.js
@@ -1,5 +1,5 @@
 const { EmbedBuilder } = require("discord.js");
-const { logMessage } = require("./logs");
+const { logMessage } = require("./log");
 
 const mirrors = [
   { 
--- forwarding/log.js
+++ forwarding/log.js
@@ -4,7 +4,7 @@ const logMessage = async (message, targetChannel, female) => {
   const logContent = `### Message transaction: \nfrom<#${message.channel.id}> (${female ? "Female Zone" : "Male Zone"}) \nto <#${targetChannel}> (${female ? "Male Zone" : "Female Zone"}) \n**User:** <@${message.author.id}> (${female ? "Female" : female === null ? "BOT" : "Male"}) \n \n**Message content:** \n${message.content}`;
 
   try {
-    const logChannel = await message.client.channels.cache.get(LOG_CHANNEL_ID);
+    const logChannel = await message.client.channels.fetch(LOG_CHANNEL_ID);
     if (logChannel) {
       await logChannel.send({
         content: logContent,
